c793d1919804b680267ccfe54a06373ff49fb5de
Mateusz Chojnowski <mateusz.chojnowski1999@wp.pl>
2024-01-11 20:36:25 +0100

    #

---
+++ SklepZKoszulkami/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SklepZKoszulkami.Repository.IRepository;
+using SklepZKoszulkami.ViewModels;
+using System.Security.Claims;
+
+namespace SklepZKoszulkami.Areas.Customer.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        [BindProperty]
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            //var claimsIdentity = (ClaimsIdentity)User.Identity;
+            //var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            //ShoppingCartVM = new()
+            //{
+            //    ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+            //    includeProperties: "Product"),
+            //    OrderHeader = new()
+            //};
+
+            //foreach (var cart in ShoppingCartVM.ShoppingCartList)
+            //{
+            //    cart.Price = GetPriceBasedOnQuantity(cart);
+            //    ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+            //}
+
+            return View(ShoppingCartVM);
+        }
+    }
+}
---
+++ SklepZKoszulkami/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using SklepZKoszulkami.Models;
+using SklepZKoszulkami.Repository.IRepository;
+using System.Diagnostics;
+
+namespace SklepZKoszulkami.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> productList = _unitOfWork.ProductRepository.GetAll();
+            return View(productList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
